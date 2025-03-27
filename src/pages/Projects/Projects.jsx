03cb526016ae367e2dfc9bb487a7d4da1475abfb
irbansin <ani.expo10@gmail.com>
2025-03-27 23:19:44 +0530

    Added Projects page

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from "./Projects.module.scss";
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>This is a placeholder for the projects page.</p>
+    </div>
+  );
+}
+export default Projects;
