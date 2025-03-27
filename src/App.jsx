196fd3bae14ec816181c21cee825b5f41dc3686e
irbansin <ani.expo10@gmail.com>
2025-03-28 02:05:25 +0530

    hiding the Menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/projects" component={Projects} />
-      <Route path="/blog" component={Blog} /> */}
+          <Route path="/projects" element={<Projects />} />
+          {/* <Route path="/blog" component={Blog} /> */}
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,19 @@
 import styles from "./Projects.module.scss";
+import { useState } from "react";
 
 function Projects() {
+  const [ProjectsList, setProjectsList] = useState([]);
+
   return (
     <div>
       <h1>Projects</h1>
       <p>This is a placeholder for the projects page.</p>
+      {ProjectsList.map((project) => (
+        <div key={project.id} className={styles.project}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
